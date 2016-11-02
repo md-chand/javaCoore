d3d85ac331610291a5382c9d1394d2e549a6deb1
Md.Chand <chand_2006wtp@yahoo.com>
2016-11-03 03:45:11 +0530

    Added employee POJO

--- src/com/orgname/javacore/oops/Employee.java
+++ src/com/orgname/javacore/oops/Employee.java
@@ -5,5 +5,46 @@ package com.orgname.javacore.oops;
  *
  */
 public class Employee {
-
+	
+	private String empName;
+	private Long empId;
+	private String adress;
+	/**
+	 * @return the empName
+	 */
+	public String getEmpName() {
+		return empName;
+	}
+	/**
+	 * @param empName the empName to set
+	 */
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	/**
+	 * @return the empId
+	 */
+	public Long getEmpId() {
+	    getEmpId();
+		return empId;
+	}
+	/**
+	 * @param empId the empId to set
+	 */
+	public void setEmpId(Long empId) {
+		this.empId = empId;
+	}
+	/**
+	 * @return the adress
+	 */
+	public String getAdress() {
+		return adress;
+	}
+	/**
+	 * @param adress the adress to set
+	 */
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
 }
